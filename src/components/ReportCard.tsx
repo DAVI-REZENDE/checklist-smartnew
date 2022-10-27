4265ef77fa8e938880e6856cea179a5053fc78d0
Davi Rezende <daviresendes12@gmail.com>
2022-10-26 22:52:49 -0300

    components report card created

--- src/components/ReportCard.tsx
+++ src/components/ReportCard.tsx
@@ -1,16 +1,16 @@
 import { Text, View } from "react-native";
-import { CaretRight } from "phosphor-react-native";
+// import { CaretRight } from "phosphor-react-native";
 
 export function ReportCard() {
   return (
-    <View>
-      <View>
-        <Text className="text-zinc-800">1 Turno - ABERTO 28 do 7 de 2022</Text>
+    <View className="flex-row items-center w-full rounded border-l-2 border-purple-500 bg-white p-4">
+      <View className="gap-2">
+        <Text className="text-zinc-800 font-bold text-base">1 Turno - ABERTO 28 do 7 de 2022</Text>
         <Text>CB1000 - TESTE - Caminhão teste 1000 Hodrometro</Text>
-        <Text>Atualizado em 28 do 7 de 2022</Text>
-        <Text>Sincronizado em 28 do 7 de 2022</Text>
+        <Text className="text-zinc-500">Atualizado em 28 do 7 de 2022</Text>
+        <Text className="text-zinc-500">Sincronizado em 28 do 7 de 2022</Text>
       </View>
-      <CaretRight size={32} color="rgb(37, 99, 235)" weight="bold" />
+      <Text>seta</Text>
     </View>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { ReportCard } from "../components/ReportCard";
 
 export function Home() {
   return (
-    <View className="flex-1 bg-zinc-100 p-4 gap-2">
+    <ScrollView className="flex-1 bg-zinc-100 p-4 gap-2">
       <ReportCard />
-    </View>
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+      <ReportCard />
+    </ScrollView>
   )
 }
